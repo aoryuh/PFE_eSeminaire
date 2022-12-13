1ff1cb774ff3ddab00580ba988349bd158c2dfad
Aoryu <damien.coq2311@gmail.com>
2022-12-13 15:01:51 +0100

    initial commit

---
+++ src/test/java/PFE/eSeminaire/ESeminaireApplicationTests.java
@@ -0,0 +1,13 @@
+package PFE.eSeminaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESeminaireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
