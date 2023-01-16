4c8200c7e539645ee69b4804c1a18eeb8d2fe471
Daryun <42786244+aoryuh@users.noreply.github.com>
2023-01-16 17:36:15 +0100

    Delete src directory

--- src/main/java/mybootapp/repo/ComposanteRepo.java
+++
@@ -1,8 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Adresse;
-import mybootapp.model.Composante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ComposanteRepo extends JpaRepository<Composante, Long> {
-}
--- src/main/java/mybootapp/repo/FormationRepo.java
+++
@@ -1,7 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Formation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FormationRepo extends JpaRepository<Formation, Long> {
-}
