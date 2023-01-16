ea81361195493846b831ba78fe6073ef9ccd08c2
Daryun <42786244+aoryuh@users.noreply.github.com>
2023-01-16 17:41:51 +0100

    Delete src directory

--- src/main/java/mybootapp/repo/AdresseRepo.java
+++
@@ -1,8 +0,0 @@
-package mybootapp.repo;
-
-import mybootapp.model.Adresse;
-import mybootapp.model.Composante;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdresseRepo extends JpaRepository<Adresse, Long> {
-}
