b0e7dc6af2dabe0ec3d7daa4775c10ab74a51eef
hallaikatia <hal.kati@yahoo.fr>
2023-01-25 00:32:56 +0100

    details du seminaire fonctionnel

--- src/main/java/PFE/eSeminaire/Controller/AppController.java
+++ src/main/java/PFE/eSeminaire/Controller/AppController.java
@@ -4,14 +4,15 @@ import PFE.eSeminaire.Service.SeminarService;
 import PFE.eSeminaire.model.Seminar;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.parameters.P;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.Collection;
+import java.util.Optional;
 
 @RequestMapping("/")
 @RestController
@@ -28,7 +29,20 @@ public class AppController {
     }
 
 
+    @RequestMapping(value = "{idSeminar}", method = RequestMethod.GET)
+    public ModelAndView viewSeminarDetails(@PathVariable("idSeminar") Long id) {
 
+        Optional<Seminar> seminar = SS.findById(id);
+
+        if (seminar.isPresent()) {
+            return new ModelAndView("detailSeminar", "seminar", seminar.get());
+
+        } else {
+            System.out.println("Error Found"); // error message
+        }
+        return null;
+
+    }
 
     @RequestMapping(value = "/myTeam", method = RequestMethod.GET)
     @PreAuthorize("hasAuthority('USER')")
