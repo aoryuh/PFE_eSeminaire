2c2ed6dffc7f51e8e3231984e026654aeeee0aa1
hallaikatia <hal.kati@yahoo.fr>
2023-02-24 13:38:33 +0100

    peuplement completé

--- src/main/java/PFE/eSeminaire/Service/PopulationService.java
+++ src/main/java/PFE/eSeminaire/Service/PopulationService.java
@@ -25,10 +25,14 @@ public class PopulationService {
     SeminarService seminarService;
 
     @Autowired
-    MessageService MS;
+    MessageService messageService;
 
     @PostConstruct
     private void init(){
+
+        /**
+         * Peuplement en team
+         */
         Team team = new Team();
         team.setName("Archive");
         ArrayList<Seminar> seminars = new ArrayList<>();
@@ -41,6 +45,9 @@ public class PopulationService {
         team.setMembers(members);
         teamService.save(team);
 
+        /**
+         * Peuplement en user
+         */
         User user = new User();
         ArrayList<String> roles = new ArrayList<>();
         roles.add("USER");
@@ -85,6 +92,9 @@ public class PopulationService {
         members.add(user);
         teamService.update(team);
 
+        /**
+         * Damien user
+         */
         user = new User();
         roles = new ArrayList<>();
         roles.add("ADMIN");
@@ -115,6 +125,7 @@ public class PopulationService {
         members.add(user);
         teamService.update(team);
 
+
         authors.add(user);
         seminar = new Seminar();
         seminar.setTitle("seminaire 2");
@@ -131,13 +142,48 @@ public class PopulationService {
         teamService.update(team);
 
 
+        /**
+         * Katia
+         */
+
         User user2 = new User();
-        roles = new ArrayList<>();
-        user2.setRoles(roles);
+        ArrayList<String> roles_user2 = new ArrayList<>();
+        roles_user2.add("USER");
+        user2.setRoles(roles_user2);
         user2.setPassword("aaa");
-        user2.setFirstName("kat");
-        user2.setName("hal");
+        user2.setFirstName("katia");
+        user2.setName("hallai");
+        user2.setMail("mailk");
+        user2.setTeam(team);
+        userService.save(user2);
+
+        members.add(user2);
+        teamService.update(team);
+
+        user2 = new User();
+        roles_user2 = new ArrayList<>();
+        roles_user2.add("ADMIN");
+        roles_user2.add("USER");
+        user2.setRoles(roles_user2);
+        user2.setPassword("aaa");
+        user2.setFirstName("Kati");
+        user2.setName("HAL");
         user2.setMail("mailad");
+        user2.setTeam(team);
+        userService.save(user2);
+
+        members.add(user2);
+        teamService.update(team);
+
+        user2 = new User();
+        roles_user2 = new ArrayList<>();
+        roles_user2.add("USER");
+        roles_user2.add("RESPO");
+        user2.setRoles(roles_user2);
+        user2.setPassword("aaa");
+        user2.setFirstName("Katiaaa");
+        user2.setName("HALLL");
+        user2.setMail("mailRespo");
         user2.setTeam(team);
         userService.save(user2);
 
@@ -155,8 +201,8 @@ public class PopulationService {
         seminar.setAuthors(authors);
         seminar.setTeam(team);
         List<String> links = new ArrayList<>();
-        String s = "etu.univ_amu.fr";
-        links.add(s);
+        String m = "etu.univ_amu.fr";
+        links.add(m);
         seminar.setOptionalContentLinks(links);
         seminars.add(seminar);
         team.setSeminars(seminars);
@@ -164,19 +210,80 @@ public class PopulationService {
         seminarService.save(seminar);
         teamService.update(team);
 
-        Message m = new Message();
-        m.setSubject("Seminaire sur la femme en informatique");
-        m.setUser(user2);
-        m.setContent("Combien de temps ça va durer ?");
-        m.setDate(new Date());
-        MS.save(m);
 
-         m = new Message();
-        m.setSubject("Seminaire sur la femme en informatique");
-        m.setUser(user);
-        m.setContent("2h");
-        m.setDate(new Date());
-        MS.save(m);
+        /**
+         * Line JAMET JAKUBIEC
+         */
+
+        User user3 = new User();
+        ArrayList<String> roles_user3 = new ArrayList<>();
+        roles_user3 = new ArrayList<>();
+        roles_user3.add("ADMIN");
+        roles_user3.add("USER");
+        user3.setRoles(roles_user2);
+        user3.setPassword("aaa");
+        user3.setFirstName("line");
+        user3.setName("JAMET JAKUBIEC");
+        user3.setMail("line.jamet@admin.univ_amu.fr");
+        user3.setTeam(team);
+        userService.save(user3);
+
+        members.add(user3);
+        teamService.update(team);
+
+        /**
+         * Peuplement de seminaires
+         */
+        for(int i=1; i<=10; i++){
+            Seminar s = new Seminar();
+            s.setTitle("La femme et l'informatique");
+            Date d = new Date();
+            s.setDate(d);
+            s.setLocation("Campus Luminy, Marseille 9éme");
+            s.setAuthors(authors);
+            s.setTeam(team);
+            List<String> l = new ArrayList<>();
+            l.add(user3.getMail());
+            seminar.setOptionalContentLinks(l);
+            String description = "Alors qu’en 1978, 50 % des étudiant·es en informatique étaient des femmes..." ;
+            s.setDescription(description);
+            seminarService.save(s);
+
+
+        }
+
+        for(int i=1; i<=5; i++){
+            Seminar ss = new Seminar();
+            ss.setTitle("L'impact de l'IA sur le monde");
+            Date dd = new Date();
+            ss.setDate(date);
+            ss.setLocation("Campus Luminy, Marseille 9éme");
+            ss.setAuthors(authors);
+            ss.setTeam(team);
+            List<String> ll = new ArrayList<>();
+            ll.add(user3.getMail());
+            ss.setOptionalContentLinks(ll);
+            String description = "L'Intelligence Artificielle (IA) est une technologie qui a un impact profond et croissant sur le monde dans de nombreux domaines..." ;
+            ss.setDescription(description);
+            seminarService.save(ss);
+        }
+
+        /**
+         * Peuplement de forum (avec des messages)
+         */
+        Message newMessage= new Message();
+        newMessage.setSubject("Seminaire sur la femme en informatique");
+        newMessage.setUser(user2);
+        newMessage.setContent("Combien de temps ça va durer ?");
+        newMessage.setDate(new Date());
+        messageService.save(newMessage);
+
+        newMessage = new Message();
+        newMessage.setSubject("Seminaire sur la femme en informatique");
+        newMessage.setUser(user);
+        newMessage.setContent("2h");
+        newMessage.setDate(new Date());
+        messageService.save(newMessage);
     }
 
 }
